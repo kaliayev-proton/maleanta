5afad3f60945a3927d751907724e7f1045253c28
kaliayev <kaliayev@protonmail.com>
2026-03-10 12:43:36 +0100

    fix: comment redirection

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,11 +2,12 @@ import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
 	const { pathname } = context.url;
+	console.log(`Middleware: ${pathname}`);
 
-	const isDev = import.meta.env.DEV;
-	if (pathname !== "/now" && !isDev) {
-		return Response.redirect(new URL("/now", context.url));
-	}
+	// const isDev = import.meta.env.DEV;
+	// if (pathname !== "/now" && !isDev) {
+	// 	return Response.redirect(new URL("/now", context.url));
+	// }
 
 	return next();
 });
