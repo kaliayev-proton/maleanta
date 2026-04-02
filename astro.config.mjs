5df9fbbba528d47b8ee2bc2284fcdfc3c053d6c3
kaliayev <kaliayev@protonmail.com>
2026-04-02 20:30:15 +0200

    fix: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 import tailwindcss from "@tailwindcss/vite";
 
@@ -10,6 +11,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [mdx()],
-  site: "https://maleanta-astro-blog.netlify.app",
+  integrations: [mdx(), sitemap()],
+  site: "https://maleanta.com",
 });
