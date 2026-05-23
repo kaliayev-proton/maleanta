4c61e29ec64f4a9d9e9493ac00b01fdeacf6a3f3
kaliayev <kaliayev@protonmail.com>
2026-05-23 18:40:08 +0200

    feat: add social media links

--- src/config/site-config.ts
+++ src/config/site-config.ts
@@ -5,6 +5,7 @@ export const siteConfig = {
 	intro:
 		"Jugar a figuras de cuerdas va sobre dar y recibir patrones; dejar caer hilos, fracasar y a veces encontrar algo que funciona, algo consecuente y quizás hasta bello, algo que antes no estaba allí; va sobre transmitir conexiones que importan, sobre contar historias con manos sobre manos, dedos sobre dedos, puntos de anclaje sobre puntos de anclaje; sobre elaborar condiciones para el florecer finito en terra, en la tierra. Las figuras de cuerdas requieren detenerse para recibir y pasar el relevo. A las figuras de cuerdas pueden jugar muchos seres, sobre todo tipo de extremidades, siempre y cuando se sostenga el ritmo de dar y recibir. La erudición y la política también son así: ir pasando algo en torsiones y madejas que requieren pasión y acción, deteniéndose y moviéndosem anclando y zarpando.",
 	author: "Donna Haraway",
-	whoWeAre:
-		"Maleanta es una editorial independiente con interés en la publicación de obras de contenido transformador que actúen como anclaje desde el que comprender la realidad e imaginar formas de emancipación colectiva.",
+	whoWeAre: `<strong>Maleanta</strong> es una editorial nacida con la voluntad de publicar textos capaces de ayudarnos a desentrañar la complejidad del presente. Nos interesa rescatar memorias no como piezas inmóviles sino como <strong>herramientas</strong> vivas.
+		
+	Frente a la sensación de aislamiento, impotencia y cansancio que caracteriza nuestro tiempo, entendemos los libros como espacios de pausa y pensamiento, elementos fundamentales para gozar del <strong>encuentro</strong> con las otras en conversaciones interminables con las que construir un <strong>afuera</strong>.`,
 };
